a230c1d8a7808ea5c0801570c7b09144ab40ab1d
Krushit-Babariya <krushitsbabariya41@gmail.com>
2025-02-09 20:56:26 +0530

    [FIX] Flow

--- Assignment-7-JDBC&SQL/src/com/narola/krushit/utils/DBConnection.java
+++ Assignment-7-JDBC&SQL/src/com/narola/krushit/utils/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
     public static Connection getConnection() throws SQLException {
         String DB_URL = "jdbc:mysql://localhost:3306/sql_my_learn";
         String DB_USER_NAME = "root";
-        String DB_PASSWORD = "password123#";
+        String DB_PASSWORD = "krushit@040";
 
         return DriverManager.getConnection(DB_URL, DB_USER_NAME, DB_PASSWORD);
     }
